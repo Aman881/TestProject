b6d262af1711bfd017162297402137648f7ed677
Aman881 <aman.rajat6@gmail.com>
2023-01-09 16:46:10 +0545

    Changes in magento and seleniumTestng

--- v1/src/test/java/com/testng/selenium/v1/SeleniumTestngTest.java
+++ v1/src/test/java/com/testng/selenium/v1/SeleniumTestngTest.java
@@ -1,31 +1,28 @@
 package com.testng.selenium.v1;
 
-import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
-import org.testng.annotations.*;
-
-public class SeleniumTestngTest {
-	public WebDriver driver;
-	public String baseUrl = "https://magento.softwaretestingboard.com/"; //magento web application
-	String driverPath ="C:\\Selenium\\chromedriver_win32\\chromedriver.exe";
-	
-	@BeforeTest
-	public void initDriver() {
-	System.setProperty("webdriver.chrome.driver", driverPath);
-	WebDriver driver=new ChromeDriver();
-	driver.manage().window().maximize();
-	driver.get(baseUrl);
+	import org.openqa.selenium.*;
+	import org.openqa.selenium.chrome.ChromeDriver;
+	import org.testng.Assert;
+	import org.testng.annotations.*;
+	public class SeleniumTestngTest {
+	    public String baseUrl = "https://magento.softwaretestingboard.com/";
+	    String driverPath = "C:\\Selenium\\chromedriver_win32\\chromedriver.exe";
+	    public WebDriver driver ; 
+	     @BeforeTest                            
+	      public void launchBrowser() {
+	          System.out.println("launching chrome browser"); 
+	          System.setProperty("webdriver.chrome.driver", driverPath);
+	          driver = new ChromeDriver();
+	          driver.get(baseUrl);
+	      }
+	      @Test                                
+	      public void verifyHomepageTitle() {
+	          String expectedTitle = "Home Page - Magento eCommerce - website to practice selenium | demo website for automation testing | selenium practice sites | selenium demo sites | best website to practice selenium automation | automation practice sites Magento Commerce - website to practice selenium | demo website for automation testing | selenium practice sites";
+	          String actualTitle = driver.getTitle();
+	          Assert.assertEquals(actualTitle, expectedTitle);
+	     }
+	      @AfterTest                           
+	      public void terminateBrowser(){
+	          driver.close();
+	      }
 	}
-	@Test
-	public void firstTest() {
-		 String expectedTitle = "Home Page - Magento eCommerce - website to practice selenium | demo website for automation testing | selenium practice sites | selenium demo sites | best website to practice selenium automation | automation practice sites Magento Commerce - website to practice selenium | demo website for automation testing | selenium practice sites";
-         String actualTitle = driver.getTitle();
-         Assert.assertEquals(actualTitle, expectedTitle);
-	}
-	
-	@AfterTest
-	public void tearDown() {
-	driver.close();
-	}
-}
--- v1/src/test/java/com/testng/selenium/v1/magnetoTest.java
+++ v1/src/test/java/com/testng/selenium/v1/magnetoTest.java
@@ -2,22 +2,80 @@ package com.testng.selenium.v1;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class magnetoTest {
-		public WebDriver driver;
-		public String testUrl ="https://magento.softwaretestingboard.com/";
-		String driverPath = "C:\\Selenium\\chromedriver_win32\\chromedriver.exe";
-		
-		@BeforeTest
-		public void launchBrowser(){
-			System.setProperty("webdriver.chrome.driver",driverPath);
-			driver = new ChromeDriver();
-			driver.get(testUrl);
+//	public WebDriver driver;
+//	public String testUrl ="https://magento.softwaretestingboard.com/";
+//	String driverPath = "C:\\Selenium\\chromedriver_win32\\chromedriver.exe";
+
+	
+		//pre conditions annotations --starting with before
+		@BeforeSuite             //1
+		public void setUp() {
+			System.out.println("setup system property for the chrome");
 		}
-	@AfterTest
-	public void closeBrowser() {
-		driver.close();
-	}
+		
+		@BeforeTest   //2
+		public void launchBrowser() {
+			System.out.println("launch chrome browser");
+		}
+		
+		@BeforeClass  //3
+		public void login(){
+			System.out.println("login to app");
+//			System.setProperty("webdriver.chrome.driver",driverPath);
+//			driver = new ChromeDriver();
+//			driver.get(testUrl);
+		}
+		
+		@BeforeMethod  //4
+		public void enterURL() {
+			System.out.println("@BeforeMethod : Enter the URL");
+		}
+		
+		@Test  //5
+		public void titleTest() {
+			System.out.println("Testcase 1 : Assert the title of the given web page");
+			
+		}
+		
+		@Test
+		public void searchTest() {
+			System.out.println("Testcase 2 : Search the results Test");
+		}
+		
+		@Test
+		public void logoTest() {
+			System.out.println("Testcase 3 : Test the logo of the application");
+		}
+		
+		@AfterMethod  //6
+		public void logOut() {
+			System.out.println("@AfterMethod :logout from application");
+		}
+		
+		@AfterClass //7
+		public void closeBrowser() {
+			System.out.println("Close the Browser");
+//		driver.close();
+		}
+	
+		@AfterTest  //8
+		public void deleteAllCookies() {
+			System.out.println("Delete all the cookies");
+		}
+		
+//		@AfterSuite  //9
+//		public void generateTestReport() {
+//		System.out.println("Generate the test report");
+//		 	}
 }
