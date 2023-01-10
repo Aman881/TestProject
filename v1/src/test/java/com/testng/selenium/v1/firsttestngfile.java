e04e05e45ef33c8cf12de77c5175ad1edfb95445
Aman881 <aman.rajat6@gmail.com>
2023-01-10 13:19:01 +0545

    test cases added

--- v1/src/test/java/com/testng/selenium/v1/firsttestngfile.java
+++ v1/src/test/java/com/testng/selenium/v1/firsttestngfile.java
@@ -8,22 +8,60 @@ package com.testng.selenium.v1;
 	    public String baseUrl = "https://www.saucedemo.com/";
 	    String driverPath = "C:\\Selenium\\chromedriver_win32\\chromedriver.exe";
 	    public WebDriver driver ; 
-	     @AfterTest                            //Jumbled
-	      public void terminateBrowser(){
-	          driver.close();
-	      }
-	     @BeforeTest                            //Jumbled
+	     @BeforeMethod                            //Jumbled
 	      public void launchBrowser() {
 	          System.out.println("launching chrome browser"); 
 	          System.setProperty("webdriver.chrome.driver", driverPath);
 	          driver = new ChromeDriver();
 	          driver.get(baseUrl);
 	      }
-	      @Test                                //Jumbled
+	     
+	      @Test                                //Testcase1
 	      public void verifyHomepageTitle() {
-	          String expectedTitle = "Welcome: Mercury Tours";
+	          String expectedTitle = "Swag Labs";
 	          String actualTitle = driver.getTitle();
-	          Assert.assertEquals(actualTitle, expectedTitle);
+	          Assert.assertEquals(actualTitle,expectedTitle);
 	     }
+	      
+	     @Test									//Testcase2
+	     public void validLogin() {
+	    	 WebElement userName = driver.findElement(By.name("user-name"));
+	         userName.click();
+	         userName.clear();
+	         userName.sendKeys("standard_user");
+	         WebElement password = driver.findElement(By.name("password"));
+	         password.click();
+	         password.clear();
+	         password.sendKeys("standard_user");
+	         WebElement submitButton = driver.findElement(By.name("login-button"));
+	         submitButton.click();
+	     }
+	     
+	     @Test
+	     public void invalidEmptyLogin() {
+	    	 driver.findElement(By.name("user-name")).clear();
+	    	 driver.findElement(By.name("password")).clear();
+	    	 driver.findElement(By.name("login-button")).click();
+	     }
+	     
+	     @Test
+	     public void invalidUsernameLogin() {
+	    	 driver.findElement(By.name("user-name")).clear();
+	    	 driver.findElement(By.name("user-name")).sendKeys("");
+	    	 driver.findElement(By.name("password")).clear();
+	    	 driver.findElement(By.name("login-button")).click();
+	     }
+	     
+	     @Test
+	     public void invalidPasswordLogin() {
+	    	 driver.findElement(By.name("user-name")).clear();
+	    	 driver.findElement(By.name("password")).clear();
+	    	 driver.findElement(By.name("login-button")).click();
+	     }
+	     
+	      @AfterMethod                           
+	      public void terminateBrowser(){
+	          driver.close();
+	      }
 	      
 	}
