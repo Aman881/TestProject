b2766ea49d5f44f07240522c58a606e1681b5b8c
Aman881 <aman.rajat6@gmail.com>
2023-01-11 17:13:57 +0545

    changes for the alert message

--- v1/src/test/java/com/testng/selenium/v1/firsttestngfile.java
+++ v1/src/test/java/com/testng/selenium/v1/firsttestngfile.java
@@ -8,7 +8,6 @@ package com.testng.selenium.v1;
 	    public String baseUrl = "https://www.saucedemo.com/";
 	    String driverPath = "C:\\Selenium\\chromedriver_win32\\chromedriver.exe";
 	    public WebDriver driver ; 
-	     @BeforeMethod                            //Jumbled
 	      public void launchBrowser() {
 	          System.out.println("launching chrome browser"); 
 	          System.setProperty("webdriver.chrome.driver", driverPath);
@@ -18,6 +17,7 @@ package com.testng.selenium.v1;
 	     
 	      @Test                                //Testcase1
 	      public void verifyHomepageTitle() {
+	    	  System.out.println("Check the test cases as expected");
 	          String expectedTitle = "Swag Labs";
 	          String actualTitle = driver.getTitle();
 	          Assert.assertEquals(actualTitle,expectedTitle);
@@ -25,6 +25,8 @@ package com.testng.selenium.v1;
 	      
 	     @Test									//Testcase2
 	     public void validLogin() {
+//	    	 try {
+	    	 System.out.println("Login with valid username and valid password");
 	    	 WebElement userName = driver.findElement(By.name("user-name"));
 	         userName.click();
 	         userName.clear();
@@ -35,28 +37,74 @@ package com.testng.selenium.v1;
 	         password.sendKeys("standard_user");
 	         WebElement submitButton = driver.findElement(By.name("login-button"));
 	         submitButton.click();
-	     }
+//	         Thread.sleep(5000);
+//	         Alert alert=driver.switchTo().alert();
+//	         String alertMessage=driver.switchTo().alert().getText();
+//	         System.out.println(alertMessage);
+//	         Thread.sleep(5000);
+//	         alert.accept();
+//	    	 }
+//	    	 catch(Exception e) {
+//	    			e.printStackTrace();
+//	    	 }
+	    	 }
+	         
 	     
 	     @Test
 	     public void invalidEmptyLogin() {
+	    	 System.out.println("Login with empty credentials");
+//	    	 try {
 	    	 driver.findElement(By.name("user-name")).clear();
 	    	 driver.findElement(By.name("password")).clear();
 	    	 driver.findElement(By.name("login-button")).click();
+//	    	 Alert alert=driver.switchTo().alert();
+//	         String alertMessage=driver.switchTo().alert().getText();
+//	         System.out.println(alertMessage);
+//	         Thread.sleep(5000);
+//	         alert.accept();
+//	    	 }catch(Exception e) {
+//	    			e.printStackTrace();
+//	    	 }
 	     }
 	     
 	     @Test
 	     public void invalidUsernameLogin() {
+//	    	 try {
+	    	 System.out.println("login with invalid username");
 	    	 driver.findElement(By.name("user-name")).clear();
-	    	 driver.findElement(By.name("user-name")).sendKeys("");
+	    	 driver.findElement(By.name("user-name")).sendKeys("abc");
 	    	 driver.findElement(By.name("password")).clear();
 	    	 driver.findElement(By.name("login-button")).click();
+//	    	 Alert alert=driver.switchTo().alert();
+//	         String alertMessage=driver.switchTo().alert().getText();
+//	         System.out.println(alertMessage);
+//	         Thread.sleep(5000);
+//	         alert.accept();
+//	    	 }
+//	    	 catch(Exception e) {
+//	    		 e.printStackTrace();
+//	    	 }
 	     }
 	     
 	     @Test
 	     public void invalidPasswordLogin() {
-	    	 driver.findElement(By.name("user-name")).clear();
-	    	 driver.findElement(By.name("password")).clear();
+//	    	 try {
+	    	 WebElement userName = driver.findElement(By.name("user-name"));
+	    	 userName.clear();
+	    	 userName.sendKeys("standard-user");
+	    	 WebElement password = driver.findElement(By.name("password"));
+	    	 password.clear();
+	    	 password.sendKeys("invalid");
 	    	 driver.findElement(By.name("login-button")).click();
+//	    	 Alert alert=driver.switchTo().alert();
+//	         String alertMessage=driver.switchTo().alert().getText();
+//	         System.out.println(alertMessage);
+//	         Thread.sleep(5000);
+//	         alert.accept();
+//	    	 }
+////	    	 catch(Exception e) {
+////	    		 e.printStackTrace();
+////	    	 }
 	     }
 	     
 	      @AfterMethod                           
